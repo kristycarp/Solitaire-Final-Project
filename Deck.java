a424528b52d89f14908ed508054acaa66e6b9af0
Kristy Carpenter <kristy.carpenter@lakesideschool.org>
2015-05-23 18:00:52 -0700

    javadocked pile and deck

--- Deck.java
+++ Deck.java
@@ -3,15 +3,56 @@ import java.awt.*;
 
 public class Deck
 {
+   /**
+     *contains all the cards in the deck, regardless of whether they are face up or down
+     */
    private ArrayList<Card> fullDeck;
-   //private ArrayList<Card> holding;
+   
+   /**
+     *contains all the cards that have not been dealt out of the deck yet. all face down.
+     */
    private Stack<Card> undealt;
+   
+   /**
+     *contains all the cards that have been dealt. only the topmost (highest index number) can be
+     *moved by the user. all face up, at most three will be visible on the screen.
+     */
    private ArrayList<Card> dealt;
-   private int x;
-   private int undealtY;
-   private int dealtY;
+   
+   /**
+     *the top left corner x value for both the dealt pile and the undealt pile
+     */
+   private final int x;
+   
+   /**
+     *the top left corner y value for the pile with the undealt cards
+     */
+   private final int undealtY;
+   
+   /**
+     *the top left corner y value for the pile with the dealt cards
+     */
+   private final int dealtY;
+   
+   /**
+     *the distance, in pixels, between the y coordinates of the top left corners of the undealt pile
+     *and the dealt pile.
+     */
    private static final int SPACING = Card.CARD_HEIGHT + 20;
    
+   /**
+     *the maximum number of cards to be dealt out from the undealt pile to the dealt pile at a time
+     */
+   private final int MAX_CARDS_DEALT = 3;
+   
+   /**
+     *the constructor for a Deck. Sets all the necessary variables, including instance variables for
+     *each Card that is added to the Deck.
+     *
+     *@param deck - all the cards that will be put into the deck
+     *@param x - the x coordinate of the top left corners of both the undealt cards and the dealt cards
+     *@param undealtY - the y coordinate of the top left corner of the undealt cards
+     */
    public Deck(ArrayList<Card> deck, int x, int undealtY)
    {
       fullDeck = deck;
@@ -30,6 +71,11 @@ public class Deck
       dealtY = undealtY + SPACING;
    }
    
+   /**
+     *deals out cards from the undealt pile to the dealt pile as the rules of solitaire dictate.
+     *if there are no more cards left in the undealt pile to deal out, all the cards from the dealt
+     *pile are moved back to the undealt pile.
+     */
    public void deal()
    {
       if (fullDeck.size() != 0)
@@ -42,9 +88,9 @@ public class Deck
                movingCard.setY(dealtY);
                dealt.set(ii, movingCard);
             }
-            if (undealt.size() > 2)
+            if (undealt.size() >= MAX_CARDS_DEALT)
             {
-               for (int jj = 1; jj <= 3; jj++)
+               for (int jj = 1; jj <= MAX_CARDS_DEALT; jj++)
                {
                   Card movingCard = undealt.pop();
                   movingCard.setY(dealtY + (jj - 1) * Card.SMALL_SUIT_HEIGHT);
@@ -79,11 +125,14 @@ public class Deck
       }
    }
    
-   /**public boolean holdingHasStuff()
-   {
-      return (holding.size() != 0);
-   }**/
-   
+   /**
+     *draws the deck by first drawing a black rectangle where the undealt pile is, then drawing each
+     *card in the deck. Because the cards' x and y coordinates and faceup status have been updated as
+     *necessary when being added to the deck or to either the dealt or undealt pile, the cards already
+     *"know" how to properly draw themselves in their deck locations.
+     *
+     *@param g - the graphics context for the drawingpanel onto which the deck will be drawn
+     */
    public void draw(Graphics g)
    {
       g.setColor(Color.BLACK);
@@ -94,6 +143,12 @@ public class Deck
       }
    }
    
+   /**
+     *gives a string representation of this deck. this representation has all the cards left in the
+     *deck in their proper order. this method is only used for debugging purposes.
+     *
+     *@return string - all the cards in the deck in their string form
+     */
    public String toString()
    {
       String string = "";
@@ -105,11 +160,20 @@ public class Deck
       return string;
    }
    
+   /**
+     *accessor method for the arraylist of dealt cards
+     *
+     *@return dealt - the arraylist of dealt cards
+     */
    public ArrayList<Card> getDealt()
    {
       return dealt;
    }
    
+   /**
+     *if it is legal to do so, this method removes the topmost (highest index) card of the dealt pile,
+     *and also removes that same card from the arraylist containing all the cards in the deck.
+     */
    public void removeTop()
    {
       if (!dealt.isEmpty())
--- Foundation.java
+++ Foundation.java
@@ -3,30 +3,58 @@ import java.awt.*;
 
 public class Foundation extends Pile //referred to as DonePile in design doc
 {
+   /**
+     *the designated suit for this foundation. set only when the first card (ace) is moved to this foundation
+     */
    private Card.Suit suit;
+   
+   /**
+     *the list of all the cards in this foundation
+     */
    private ArrayList<Card> cardList;
    
+   /**
+     *the constructor for a Foundation
+     *
+     *@param x - the x coordinate of the top left corner of this foundation
+     *@param y - the y coordinate of the top left corner of this foundation
+     */
    public Foundation(int x, int y)
    {
       super(x, y);
       cardList = new ArrayList<Card>();
    }
    
+   /**
+     *the default constructor for a Foundation. Creates a Foundation in the top left corner of the panel
+     */
    public Foundation()
    {
       this(0, 0);
    }
-   
-   //this method assumes that the Cards are being correctly added to the stack of cards, i.e. that
-   //if a King is added then all the other cards have been added correctly
+
+   /**
+     *this method checks to see if this foundation has been completed. it assumes that the Cards are
+     *being correctly added to the list of cards, i.e. that if a King is added then all the other
+     *cards preceding it have been added correctly
+     *
+     *@return true if this foundation is complete, false if it is not
+     */
    public boolean isComplete()
    {
       if (!cardList.isEmpty() && cardList.get(cardList.size() - 1).getValue() == 13)
+      {
          return true;
+      }
       return false;
    }
    
-   //returns true if successful add, false if unsuccessful
+   /**
+     *if it is legal, this method adds a card to the foundation and changes the necessary variables to
+     *truly add the card
+     *
+     *@param c - the card to be added
+     */
    public void addCard(Card c)
    {
       if (c.getValue() == 1 && cardList.isEmpty())
@@ -36,8 +64,6 @@ public class Foundation extends Pile //referred to as DonePile in design doc
          c.setY(y);
          cardList.add(c);
          suit = c.getSuit();
-         //System.out.println("I added an ace");
-         //return true;
       }
       else if (!cardList.isEmpty() && c.getSuit().equals(suit) && c.getValue() == cardList.get(cardList.size() - 1).getValue() + 1)
       {
@@ -45,12 +71,15 @@ public class Foundation extends Pile //referred to as DonePile in design doc
          c.setX(x);
          c.setY(y);
          cardList.add(c);
-         //System.out.println("I added " + c.fullToString());
-         //return true;
       }
-      //return false;
    }
    
+   /**
+     *this method checks to see if a given card can be legally added to this foundation
+     *
+     *@param c - the card to be checked
+     *@return true if the card can be legally added, false otherwise
+     */
    public boolean canAddCard(Card c)
    {
       if (c.getValue() == 1 && cardList.isEmpty())
@@ -64,8 +93,12 @@ public class Foundation extends Pile //referred to as DonePile in design doc
       return false;
    }
    
-   //will throw exception if no cards to remove, so make sure you call it after making sure there is
-   //something to remove
+   /**
+     *this method removes the topmost (highest value and highest index) card in the cardList of this
+     *foundation. if the cardList is empty, it throws an IndexOutOfBounds exception
+     *
+     *@return the card that was removed
+     */
    public Card removeCard()
    {
       if (!cardList.isEmpty())
@@ -78,11 +111,21 @@ public class Foundation extends Pile //referred to as DonePile in design doc
       }
    }
    
+   /**
+     *accessor method for the list of all cards in the foundation
+     *
+     *@return cardList - all the cards in the foundation, in order from least to greatest value
+     */
    public ArrayList<Card> getCards()
    {
       return cardList;
    }
    
+   /**
+     *accessor method for the y coordinate of the top left corner of this foundation
+     *
+     *@return y - the top left y coordinate
+     */
    public int getY()
    {
       return y;
